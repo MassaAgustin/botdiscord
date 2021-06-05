a57c2505bf18227c36913afb9ef0c88635fbca7f
AgusMassa <agus.massa@hotmail.com>
2021-06-05 14:17:57 -0300

    hotfix cuantosfaltan

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ client.on('message', (message) => {
     if (content === 'cuantos faltan') {
         let cantFaltan = 10 - jugadores.length;
         const description = `${cantFaltan} platitas`
-        mensaje.setTitle('Vamooo que faltan: ', 'RED', description);
+        mensaje.setMensaje('Vamooo que faltan: ', 'RED', description);
         message.channel.send(mensaje)
     }
 })
