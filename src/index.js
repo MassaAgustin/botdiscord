6f706ab85af90c180aaa919990530c72f702a7ce
AgusMassa <agus.massa@hotmail.com>
2021-06-05 06:35:11 -0300

    metodo para crear equipo añadido

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
+const { Client, MessageEmbed, MessageCollector } = require('discord.js');
+const client = new Client();
+
+const config = require("../config.json");
+
+var jugadores = [];
+var equipo1 = [];
+var equipo2 = [];
+
+const mensaje = new MessageEmbed();
 
 client.on('ready', () => {
     console.log('Bot is ready as', client.user.tag);
@@ -7,11 +15,83 @@ client.on('ready', () => {
 
 client.on('message', (message) => {
 
-    if (message.content === 'se pica') {
-        message.reply('Se re pica :hot_face:');
-        message.delete();
+    const { content, reply } = message;
+
+    if (content === 'se pica') {
+
+        if (jugadores.length === 10) {
+            jugadores = []
+            equipo1 = []
+            equipo2 = []
+        }
+
+        setMensaje('Se re pica :hot_face:', 'RED', 'Escriban "quiero fedear" los que quieran jugar')
+        message.channel.send(mensaje);
     }
 
+    if (content === 'quiero fedear') {
+
+        const nickJugador = message.author.tag;
+        if (jugadores.length !== 10) {
+            if (!existeJugador(nickJugador)) {
+                jugadores.push(nickJugador);
+                setMensaje('Cargando Equipo', 'RED', `${jugadores.length}/10`)
+                message.channel.send(mensaje);
+            } else {
+                setMensaje('No vale repetir! ', 'RED', nickJugador);
+                message.channel.send(mensaje);
+            }
+
+
+        } else {
+            getEquiposRandom();
+            setMensaje('Listo rancios ya estamos los 10', 'RED', 'Ahora les paso los equipos');
+            console.log(equipo1);
+            console.log(equipo2);
+            message.channel.send(mensaje)
+        }
+    }
+
+    if (content.includes('faltan')){
+        const cantFaltan = 10 - jugadores.length;
+        setTitle('Vamooo que faltan: ', 'RED', `${cantFaltan}`);
+        message.channel.send(mensaje)
+    }
 })
 
-client.login('ODUwNjI5ODg5Mjg2OTk1OTg4.YLsg0Q.hsoIMsU2LHDPiD1ZBDAJWvuMD_I');
+const setMensaje = (title, color, description) => {
+    mensaje.setTitle(title);
+    mensaje.setColor(color);
+    mensaje.setDescription(description)
+}
+
+const existeJugador = (jugador) => {
+    let existe = false;
+    jugadores.forEach(player => {
+        if (player === jugador) {
+            existe = true
+        }
+    })
+
+    return existe;
+}
+
+const getEquiposRandom = () => {
+
+    while (equipo1.length != 5) {
+        const random = (Math.random() * 10);
+
+        if (jugadores[random] !== 'listo') {
+            equipo1.push(jugadores[random]);
+            jugadores[random] = 'listo'
+        }
+    }
+
+    jugadores.forEach((jugador, index) => {
+        if (jugador !== 'listo') {
+            equipo2.push(jugador)
+        }
+    })
+}
+
+client.login(config.token);
