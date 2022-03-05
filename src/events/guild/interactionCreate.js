59baa5acbd72bb9ee86eba3ec81b3cdb671307df
Agustin Massa <amassa@sinergiasoluciones.com.ar>
2022-03-05 17:12:37 -0300

    15

--- src/events/guild/interactionCreate.js
+++ src/events/guild/interactionCreate.js
@@ -1,9 +1,7 @@
-const wait = require('util').promisify(setTimeout);
 
 module.exports = async (client, discord, interaction) => {
 
     if (!interaction.isCommand()) return;
-    if (!interaction.isGuild()) return interaction.reply('Este comando solo puede ser utilizado en un server');
 
     const interactionCommand = client.slash.get(interaction.commandName);
 
@@ -14,6 +12,4 @@ module.exports = async (client, discord, interaction) => {
     } catch (err) {
         console.log('Error al ejecutar el comando slash: ', err);
     }
-
-
 }
